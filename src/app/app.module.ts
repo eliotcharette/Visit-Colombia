57b0f3f9c2c65150dd102d48f3e117068033f7ca
Guest User <Guest@epicodus-9.local>
2018-08-24 14:50:29 -0700

    Add styles to footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { ColombiaHeaderComponent } from './colombia-header/colombia-header.component';
 import { ColombiaNavComponent } from './colombia-nav/colombia-nav.component';
 import { ColombiaCarouselComponent } from './colombia-carousel/colombia-carousel.component';
 import { ColombiaWelcomeComponent } from './colombia-welcome/colombia-welcome.component';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { NewExperienceComponent } from './new-experience/new-experience.component';
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { ExperienceComponent } from './experience/experience.component';
 import { AdminComponent } from './admin/admin.component';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
   authDomain: masterFirebaseConfig.authDomain,
   databaseURL: masterFirebaseConfig.databaseURL,
-  storageBucket: masterFirebaseConfig.storageBucket
+  storageBucket: masterFirebaseConfig.storageBucket,
+  messagingSenderId: masterFirebaseConfig.messagingSenderId
 };
 
 @NgModule({
@@ -39,7 +41,8 @@ export const firebaseConfig = {
     HttpModule,
     routing,
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
